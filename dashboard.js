97d2ccf18546a2db13021bc09f185dff1516a37c
js0094q <joe@stewartinbox.com>
2025-12-04 08:18:19 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       window.AppState.setEvents(events);
       renderEvents(events);
-    } catch (error) {
+      catch (error) {
       console.error(error);
       eventsContainer.innerHTML =
         '<div class="error">Failed to load games. Check API Key or quota.</div>';

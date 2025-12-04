ef8d6f40dfba9ebdd321771da39285ba15b25e21
js0094q <joe@stewartinbox.com>
2025-12-04 08:34:16 -0500

    Update events.js

--- api/events.js
+++ api/events.js
@@ -5,13 +5,16 @@
 
 export default async function handler(req, res) {
   const apiKey = process.env.ODDS_API_KEY;
+  const sport = "americanfootball_nfl";
+  const regions = "us";
+  const oddsFormat = "american";
+  const eventId = req.query.eventId;
 
-  if (!apiKey) {
-    return res.status(500).json({ error: "Missing ODDS_API_KEY in backend env" });
+  if (!apiKey || !eventId) {
+    return res.status(400).json({ error: "Missing apiKey or eventId" });
   }
 
-  const sport = "americanfootball_nfl";
-  const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${apiKey}&regions=us&markets=h2h,spreads&oddsFormat=american`;
+  const url = `https://api.the-odds-api.com/v4/sports/${sport}/events/${eventId}/odds?apiKey=${apiKey}&regions=${regions}&markets=h2h,totals&oddsFormat=${oddsFormat}`;
 
   try {
     const r = await fetch(url, { cache: "no-store" });

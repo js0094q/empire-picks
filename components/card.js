eb34233e34d7879014b2f2b72fca286b2d2b5a24
js0094q <joe@stewartinbox.com>
2025-12-04 07:50:27 -0500

    Update card.js

--- components/card.js
+++ components/card.js
@@ -1,18 +1,13 @@
 import { toggleAccordion } from "./ui.js";
-import { LinesPanel } from "./components/linesPanel.js";
-import { PropsPanel } from "./components/propsPanel.js";
+import { LinesPanel } from "./linesPanel.js";
+import { PropsPanel } from "./propsPanel.js";
 
 export const CardComponent = {
   create(event) {
-    const homeAssets = window.TeamAssets.get(event.home.name);
-    const awayAssets = window.TeamAssets.get(event.away.name);
-
     const card = document.createElement("div");
     card.className = "game-card";
 
     card.innerHTML = `
-      <div class="team-bar" style="background:${homeAssets.color}"></div>
-
       <div class="game-header" onclick="toggleAccordion(this.parentElement)">
         <span>${event.away.name} @ ${event.home.name}</span>
         <span>${event.time}</span>
@@ -20,27 +15,14 @@ export const CardComponent = {
 
       <div class="team-row">
         <span class="team-name">${event.away.name}</span>
-        <span class="score-area">-</span>
-        <div class="odds-group">
-          <button class="odd-btn" 
-            onclick="window.addLeg(event,'${event.id}','Spread','${event.odds.away}')">
-            ${event.odds.away}
-          </button>
-        </div>
+        <span>${event.odds.away}</span>
       </div>
 
       <div class="team-row">
         <span class="team-name">${event.home.name}</span>
-        <span class="score-area">-</span>
-        <div class="odds-group">
-          <button class="odd-btn"
-            onclick="window.addLeg(event,'${event.id}','Spread','${event.odds.home}')">
-            ${event.odds.home}
-          </button>
-        </div>
+        <span>${event.odds.home}</span>
       </div>
 
-      <!-- EXPANDING CARD BODY -->
       <div class="card-expanded">
         <div class="panel-tabs">
           <button class="tab-btn active" onclick="LinesPanel.show('${event.id}', this)">Lines</button>
@@ -51,7 +33,7 @@ export const CardComponent = {
           <div class="loader">Loading lines...</div>
         </div>
 
-        <div class="tab-content" id="props-${event.id}" style="display:none;">
+        <div class="tab-content" id="props-${event.id}" style="display:none">
           <div class="loader">Loading props...</div>
         </div>
       </div>

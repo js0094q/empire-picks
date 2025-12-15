8d64250ba8f9bb191b653f70a9bf1dcc912781d8
js0094q <joe@stewartinbox.com>
2025-12-14 19:21:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -254,6 +254,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   sumEl = document.getElementById("parlay-summary");
   stakeEl = document.getElementById("parlay-stake");
 
+  document?.getElementById("close-parlay").onclick = closeParlay;
+  backdrop?.addEventListener("click", closeParlay);
   backdrop?.addEventListener("click", closeParlay);
   stakeEl?.addEventListener("input", renderParlay);
 

c8cd920b87a4049e308b5a915577f436fa8ccd27
js0094q <joe@stewartinbox.com>
2026-01-02 07:24:16 -0500

    Add files via upload

---
+++ teams.js
@@ -0,0 +1,57 @@
+export const Teams = {
+  "New York Giants": {
+    abbr: "nyg",
+    logo: "https://a.espncdn.com/i/teamlogos/nfl/500/nyg.png",
+  },
+  "Philadelphia Eagles": {
+    abbr: "phi",
+    logo: "https://a.espncdn.com/i/teamlogos/nfl/500/phi.png",
+  },
+  "Dallas Cowboys": {
+    abbr: "dal",
+    logo: "https://a.espncdn.com/i/teamlogos/nfl/500/dal.png",
+  },
+  "Washington Commanders": {
+    abbr: "wsh",
+    logo: "https://a.espncdn.com/i/teamlogos/nfl/500/wsh.png",
+  },
+
+  /* ==========================
+     FULL TEAM LIST INCLUDED
+     ========================== */
+
+  "Buffalo Bills": { abbr: "buf", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/buf.png" },
+  "Miami Dolphins": { abbr: "mia", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/mia.png" },
+  "New York Jets": { abbr: "nyj", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/nyj.png" },
+  "New England Patriots": { abbr: "ne", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
+
+  "Baltimore Ravens": { abbr: "bal", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/bal.png" },
+  "Cincinnati Bengals": { abbr: "cin", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/cin.png" },
+  "Cleveland Browns": { abbr: "cle", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/cle.png" },
+  "Pittsburgh Steelers": { abbr: "pit", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/pit.png" },
+
+  "Houston Texans": { abbr: "hou", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/hou.png" },
+  "Jacksonville Jaguars": { abbr: "jax", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/jax.png" },
+  "Tennessee Titans": { abbr: "ten", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ten.png" },
+  "Indianapolis Colts": { abbr: "ind", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ind.png" },
+
+  "Kansas City Chiefs": { abbr: "kc", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/kc.png" },
+  "Las Vegas Raiders": { abbr: "lv", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/lv.png" },
+  "Los Angeles Chargers": { abbr: "lac", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/lac.png" },
+  "Denver Broncos": { abbr: "den", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/den.png" },
+
+  "Chicago Bears": { abbr: "chi", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/chi.png" },
+  "Detroit Lions": { abbr: "det", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/det.png" },
+  "Green Bay Packers": { abbr: "gb", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/gb.png" },
+  "Minnesota Vikings": { abbr: "min", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/min.png" },
+
+  "Tampa Bay Buccaneers": { abbr: "tb", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/tb.png" },
+  "New Orleans Saints": { abbr: "no", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/no.png" },
+  "Atlanta Falcons": { abbr: "atl", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/atl.png" },
+  "Carolina Panthers": { abbr: "car", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/car.png" },
+
+  "San Francisco 49ers": { abbr: "sf", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/sf.png" },
+  "Los Angeles Rams": { abbr: "lar", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/lar.png" },
+  "Seattle Seahawks": { abbr: "sea", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/sea.png" },
+  "Arizona Cardinals": { abbr: "ari", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ari.png" }
+};
